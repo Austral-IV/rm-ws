9214d303608317b62a56d7309e662bff06d8d8ad
Juanti <jvargasf@uc.cl>
2022-06-21 20:24:03 -0400

    ayudantia como base e intento de filtro bayes

--- src/lab-3/scripts/sensor_model.py
+++ src/lab-3/scripts/sensor_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from numbers import Integral
 import numpy as np
 from scipy.ndimage.filters import convolve
 from filterpy.discrete_bayes import predict, update, normalize
@@ -7,35 +8,29 @@ belief0 = 1 / (map_dim[0]*map_dim[1]) * np.ones(map_dim) # prior: inicialmente h
 likelyhood = 1 / (map_dim[0]*map_dim[1]) * np.ones(map_dim) # prior: inicialmente hay prob uniforme en todo el mapa
 belief0[3,3] = 1
 belief0 = normalize(belief0)
-# def bayes_filter
+
+#------------------------ WIP-----------------------------------
 
 
+resolution=0.01
+zmax = 4
+distances = int(zmax/resolution)
+prior = np.ones(distances)/distances
+def laser_range_filter(prior, ranges, range_accuracy = 0.9):
+    X = len(prior)
+    T = len(ranges)
+    Z = np.ones(X)*range_accuracy
+    px_u = np.zeros(X)
 
-def gkern(l=5, sig=1.):
-    """https://stackoverflow.com/questions/29731726/how-to-calculate-a-gaussian-kernel-matrix-efficiently-in-numpy
-    creates gaussian kernel with side length `l` and a sigma of `sig`
-    """
-    ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
-    gauss = np.exp(-0.5 * np.square(ax) / np.square(sig))
-    kernel = np.outer(gauss, gauss)
-    return kernel / np.sum(kernel)
+    for t in range(T):
+        #no hay movimiento, por lo que p(x|u,xt-1) = 1
+        bel_bar = np.sum(prior)
+        # hacer tq pz_x
+        #nu = 1/range_acc*la distancia medida + (1-r_acc*el resto)
+        nu = 1/(range_accuracy*prior(range(t)) + np.sum((1-range_accuracy)*prior) 
+            - (1-range_accuracy)*prior(range(t)))
 
+        belt = nu*Z*bel_bar
+        prior = belt
 
-
-def predict_move(prior, movement, kernel):
-    """
-    Predice el estado del mapa a partir del estado actual y el movimiento.
-    El kernel indica la relación re prob over/undershoot
-    """
-    return convolve(np.roll(prior, movement), kernel, mode='wrap')
-
-
-
-# asumimos que el movimiento tiene una distribución gausiana
-mov_kernel = gkern(l=3, sig = 0.5)
-prior = predict(belief0, [1,1], mov_kernel, mode = 'wrap')
-posterior = update(likelyhood, prior)
-belief0 = posterior.round(1) ; belief0[np.isnan(posterior)] = 0 ; belief0[posterior < 0] = 0 ; belief0
-
-
-
+    return belt
