6ebf64e7ed476fc58b0a6caeea1cef694a179097
Juanti <jvargasf@uc.cl>
2022-06-28 17:50:53 -0400

    bayes_filter depracated

--- src/lab-3/scripts/bayes_filter.py
+++ src/lab-3/scripts/bayes_filter.py
@@ -1,16 +1,16 @@
 import numpy as np
 from likelihood_fields import Localizacion
-# arr = np.array([[1, 2, 3], [1, 3, 1]])
-# result = np.where(arr == max(arr))[0]
-# print(result)
+import time
 
 class BayesFilter():
     loc = Localizacion()
+    zmax, pxperm = (4, 100)
+    X = zmax*pxperm
     def __init__(self, arr, accuracy = 0.9):
-        self.prior = np.ones(len(arr))/len(arr) #inic. prob uniforme
+        self.prior = np.ones([len(arr), self.X])/len(arr) #inic. prob uniforme
         self.z = accuracy # ranger_accuracy
         self.ranges = np.array([])
-    
+        self.time = 0
     def update_belief(self, zt, prior):
         # quizá self.z debería ser likelihood_model(zt, x)
         # correct_scale = self.z/(1-self.z)
@@ -19,19 +19,24 @@ class BayesFilter():
 
         """ lo mismo pero usando la función de likelihood de daniel: """
         lh = np.array([])
+        self.timer_st()
         for i in prior:
+            # calcula el likelyhood para cada distancia
             q = self.loc.likelihood_fields_model(zt, [0, 0, 0])
             np.append(lh, q/(1-q))
+        self.timer_end("tiempo en calc likelihoods: ")
         
         return (lh*prior) / sum(lh*prior)
-            
+    
+    def timer_st(self):self.time = time.time()
+    def timer_end(self, msg=None): 
+        print(msg, time.time() - self.time)
             
 
     def bayes_filter(self, zt, ut=1):
         """ para cada valor de distancia entregado debemos calcular el belief
         ut (i.e. acción) es la noacción: p(xt|ut, xt-1) = 1 si xt=xt-1 si no 0"""
-        zmax, pxperm = (4, 100)
-        X = zmax*pxperm
+        X = self.X
         bel = []
         correct_scale = self.z*10
         posterior = np.array([])
@@ -51,5 +56,5 @@ if __name__ == '__main__':
     z = [0.521]     # Distancia real: 0.52
     x = [1.18, 1.36, 0]
     filter = BayesFilter(x)
-
-    print(filter.loc.likelihood_fields_model(z, x))
+    filter.bayes_filter([z, z, z])
+    
