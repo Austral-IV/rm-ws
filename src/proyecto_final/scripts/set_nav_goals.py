0ed8bc8c12cee241a8ee0065125e80d78a85792c
Juanti <jvargasf@uc.cl>
2022-07-09 19:12:36 -0400

    terminado set_nav_goals

--- src/proyecto_final/scripts/set_nav_goals.py
+++ src/proyecto_final/scripts/set_nav_goals.py
@@ -6,37 +6,40 @@ import actionlib
 from geometry_msgs.msg import Pose
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-#  creo que la orientación se da en quaternions
-GOALS = {
-    "pos" : [[11.5, 4.2], [20, 17], [30.38, 6.2]], 
-    "orient" : [[0, 0, -0.6, 0.8], [0, 0, -0.05, 1], [0, 0, 0.2, 1]]
-    }
 
-def goal_done( status, result ):
-    global goal
-    rospy.loginfo( 'DONE status: %s (%s)', move_base_client.get_goal_status_text(), str( status ) )
-    if goal >= 2: 
-        Done = True
-    else: goal += 1
+
     
+class GoalSender():
+    def __init__(self):
 
+        #  creo que la orientación se da en quaternions
+        self.GOALS = {
+            "pos" : [[11.5, 4.2], [20, 17], [30.38, 6.2]], 
+            "orient" : [[0, 0, -0.6, 0.8], [0, 0, -0.05, 1], [0, 0, 0.2, 1]]
+            }
 
-if __name__ == '__main__':
-    rospy.init_node( 'nav_stack_example' )
-
-    Done = False
-
-    move_base_client = actionlib.SimpleActionClient( '/move_base', MoveBaseAction )
-    move_base_client.wait_for_server()
+        rospy.init_node( 'nav_stack_example' )
+        self.move_base_client = actionlib.SimpleActionClient( '/move_base', MoveBaseAction )
+        self.move_base_client.wait_for_server()
+        self.move_base_goal = MoveBaseGoal()
+        self.move_base_goal.target_pose.header.frame_id = 'map'
+        self.move_base_goal.target_pose.header.stamp = rospy.Time.now()
     
-    move_base_goal = MoveBaseGoal()
-    move_base_goal.target_pose.header.frame_id = 'map'
-    move_base_goal.target_pose.header.stamp = rospy.Time.now()
+    def run(self):
+        self.goal = 0
+        self.set_goal()
+    
+    def goal_done(self, status, result):
+        # rospy.loginfo( 'DONE status: %s (%s)', self.move_base_client.get_goal_status_text(), str( status ) )
+        rospy.loginfo( "Reached Goal:", self.GOALS["pos"][self.goal])
+        if self.goal >= 2: pass
+        self.goal += 1
+        self.set_goal()
 
-    for goal in range(2):
-        
-        position= GOALS["pos"][goal]
-        orientation= GOALS["orient"][goal]
+    def set_goal(self):
+        goal = self.goal
+        position= self.GOALS["pos"][goal]
+        orientation= self.GOALS["orient"][goal]
         goal_pose = Pose()
 
         goal_pose.position.x = position[0]
@@ -46,7 +49,17 @@ if __name__ == '__main__':
         goal_pose.orientation.z = orientation[2]
         goal_pose.orientation.w = orientation[3]
 
-        move_base_goal.target_pose.pose = goal_pose
-        move_base_client.send_goal( move_base_goal, done_cb = goal_done )
+        self.move_base_goal.target_pose.pose = goal_pose
+        self.move_base_client.send_goal( self.move_base_goal, done_cb = self.goal_done )
+
+
+if __name__ == '__main__':
+    rospy.init_node( 'nav_stack_example' )
+    
+    Done = False
+    reached_goal = False
+
+    goalsender = GoalSender()
+    goalsender.run()
 
     rospy.spin()
